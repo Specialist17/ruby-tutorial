8c5b112ebc7487d7b1559bd1505f557b48a04c42
Specialist17 <arocho.fernando.13@gmail.com>
2018-01-21 15:18:34 -0800

    testing user and memos

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     end
 
     # Salts and hashes a user's password
-    def encrypt_password do
+    def encrypt_password 
         if password.present?
             self.password_salt = BCrypt::Engine.generate_salt
             self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
--- db/migrate/20180116042722_add_password_and_toke_to_users.rb
+++ db/migrate/20180116042722_add_password_and_toke_to_users.rb
@@ -2,7 +2,7 @@ class AddPasswordAndTokeToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :token, :string
     add_index :users, :token
-    add_column :users, :password_hash, :string,
+    add_column :users, :password_hash, :string
     add_column :users, :password_salt, :string
   end
 end
