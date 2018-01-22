446c1083da82f196fc935fd6585967e92c6cb38e
Specialist17 <arocho.fernando.13@gmail.com>
2018-01-21 17:39:38 -0800

    added authentication and aws gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,21 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it "is valid with valid attributes" do
-      user = User.new(name: "Eliel", email: "eliel@test.com")
+      user = User.new(name: "Eliel", email: "eliel@test.com", password: "test")
       expect(user).to be_valid
     end
 
     it "is invalid without a name" do
-      bad_user = User.new(name: nil, email: "test@mail.com")
+      bad_user = User.new(name: nil, email: "test@mail.com", password: "test")
       expect(bad_user).to_not be_valid
     end
 
     it "is invalid without an email" do
-      bad_user = User.new(name: "Eliel", email: nil)
+      bad_user = User.new(name: "Eliel", email: nil, password: "test")
+      expect(bad_user).to_not be_valid
+    end
+    it "is invalid without a password" do
+      bad_user = User.new(name: "Eliel", email: nil, password: nil)
       expect(bad_user).to_not be_valid
     end
   end
